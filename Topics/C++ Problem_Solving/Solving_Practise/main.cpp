a303d6eba307073ea5788535165bab5a3cc52b87
Hazem Ayman <hazemAyman319>
2024-01-29 21:16:23 +0200

    new update

--- Topics/C++ Problem_Solving/Solving_Practise/main.cpp
+++ Topics/C++ Problem_Solving/Solving_Practise/main.cpp
@@ -3,36 +3,26 @@
 using namespace std;
 #define ll long long
 const int N = 1e5;
-struct Point{
-    int x;
-    int y;
-};
-int n;
-Point A[N]; //array of structs (2D-points)
-//true -> don't swap
-//false -> swap
-bool cmp(Point a , Point b)
-{
-    if(a.x == b.x)  // if their x's are equal then compare with y else then compare with x
-    {
-        return a.y < b.y;
-    }
-    return a.x < b.x;
-}
+int n, x;
+vector<int> V;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
     cin>>n;
-    for(int i=0; i < n; i++)
-    {
-        cin>>A[i].x >> A[i].y;
+    for(int i=0; i<n; i++){
+        cin>>x;
+        v.push_back(x);
     }
-    sort(A , A+n , cmp);
+    sort(v.begin() , v.end());
     for(int i=0; i<n; i++)
     {
-        cout<<A[i].x<<" "<<A[i].y<<endl;
+        cout<<v[i]<<endl;
+    }
+    for(int x : v){
+        cout << x <<endl;
     }
     return 0;
 }
