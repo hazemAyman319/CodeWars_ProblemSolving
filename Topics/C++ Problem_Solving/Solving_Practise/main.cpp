0dd8640f73f47eaa548a51af409ff322afbb9fe1
Hazem Ayman <hazemAyman319>
2024-02-08 12:36:08 +0200

    new update

--- Topics/C++ Problem_Solving/Solving_Practise/main.cpp
+++ Topics/C++ Problem_Solving/Solving_Practise/main.cpp
@@ -2,87 +2,43 @@
 #include <bits\stdc++.h>
 using namespace std;
 #define ll long long
-int q , n;
-string command;
-bool isReversed;
-vector<int>v;
-deque<int>dq;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    cin>>q;
-    while(q--)
+    while(cin>>n , n)
     {
-        cin>>command;
-        if(command == "back")
+        queue<int>q;
+        for(int i=0; i<n; i++)
         {
-            if(dq.empty())
+            cin>>x;
+            q.push(x);
+        }
+        stack<int>st;
+        int next = 1;
+        while(!q.empty() || (!st.empty() && st.top()==next))
+        {
+            if(!q.empty() && q.front()==next)
             {
-                cout<<" No Job For Ada! "<<endl;
+                q.pop();
+                next++;
             }
-            else
-            {
-                if(isReversed)
-                {
-                    cout<<dq.front()<<endl;
-                    dq.pop_front();
-                }
-                else
-                {
-                    cout<<dq.back()<<endl;
-                    dq.pop_back();//O(1)
-                }
+            else if(!st.empty() && st.top()==next){
+                st.pop();
+                next++;
+            }
+            else if(!q.empty()){
+                st.push(q.front());
+                q.pop();
             }
         }
-        else if(command == "front")
-        {
-            if(dq.empty())
-            {
-                cout<<" No Job For Ada! "<<endl;
-            }
-            else
-            {
-                if(isReversed)
-                {
-                    cout<<dq.back()<<endl;
-                    dq.pop_back();
-                }
-                else
-                {
-                    cout<<dq.front()<<endl;
-                    dq.pop_front();//O(1)
-                }
-            }
-        }
-        else if(command == "reverse")
-        {
-            isReversed = !isReversed;
-        }
-        else if(command == "push_back")
-        {
-            cin>>n;
-            if(isReversed)
-            {
-                dq.push_front(n);
-            }
-            else
-            {
-                dq.push_back(n);
-            }
+        if(next == n + 1){
+            cout<<"yes"<<endl;
         }
         else
         {
-            cin>>n;
-            if(isReversed)
-            {
-                dq.push_back(n);
-            }
-            else
-            {
-                dq.push_front(n);
-            }
+            cout<<"no"<<endl;
         }
     }
     return 0;
